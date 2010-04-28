ab6e820b88d0d2ce72a9b8a3ba1b2c8f15831f45
cubicdaiya <cubicdaiya@gmail.com>
2010-04-28 20:39:43 +0900

    change indent size

--- mpool.c
+++ mpool.c
@@ -1,34 +1,34 @@
 /**
- In short, mpool is distributed under so called "BSD license",
- 
- Copyright (c) 2009-2010 Tatsuhiko Kubo <cubicdaiya@gmail.com>
- All rights reserved.
- 
- Redistribution and use in source and binary forms, with or without modification,
- are permitted provided that the following conditions are met:
- 
-    * Redistributions of source code must retain the above copyright notice,
-    this list of conditions and the following disclaimer.
- 
-    * Redistributions in binary form must reproduce the above copyright notice,
-    this list of conditions and the following disclaimer in the documentation
-    and/or other materials provided with the distribution.
- 
-    * Neither the name of the authors nor the names of its contributors
-    may be used to endorse or promote products derived from this software 
-    without specific prior written permission.
- 
- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
- TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+   In short, mpool is distributed under so called "BSD license",
+   
+   Copyright (c) 2009-2010 Tatsuhiko Kubo <cubicdaiya@gmail.com>
+   All rights reserved.
+   
+   Redistribution and use in source and binary forms, with or without modification,
+   are permitted provided that the following conditions are met:
+   
+   * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+   
+   * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+   
+   * Neither the name of the authors nor the names of its contributors
+   may be used to endorse or promote products derived from this software 
+   without specific prior written permission.
+   
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+   TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 /* written by C99 style */
@@ -45,55 +45,55 @@ static size_t mpool_align(size_t siz);
  * create memory pool
  */
 mpool_t *mpool_create (size_t siz) {
-  if (siz <= 0) {
-    siz = MPOOL_DEFAULT_SIZE;
-  } else {
-    siz = mpool_align(siz);
-  }
-  mpool_t *p;
-  MPOOL_MALLOC(p, sizeof(mpool_t));
-  MPOOL_MALLOC(p->pool, siz);
-  p->begin = p->pool;
-  p->usiz  = 0;
-  p->msiz  = siz;
-  p->head  = p;
-  p->next  = NULL;
-  return p;
+    if (siz <= 0) {
+        siz = MPOOL_DEFAULT_SIZE;
+    } else {
+        siz = mpool_align(siz);
+    }
+    mpool_t *p;
+    MPOOL_MALLOC(p, sizeof(mpool_t));
+    MPOOL_MALLOC(p->pool, siz);
+    p->begin = p->pool;
+    p->usiz  = 0;
+    p->msiz  = siz;
+    p->head  = p;
+    p->next  = NULL;
+    return p;
 }
 
 /**
  * allocate memory from memory pool
  */
 mpool_pool_t *mpool_alloc(mpool_t **p, size_t siz) {
-  mpool_t *pp = *p;
-  size_t usiz = mpool_align(pp->usiz + siz);
-  size_t msiz = pp->msiz;
-  mpool_pool_t *d = pp->begin;
-  if (usiz > msiz) {
-    mpool_extend(pp, usiz * 2);
-    pp->next->usiz = usiz;
-    d = pp->next->begin;
-    pp->next->begin += siz;
-    *p = pp->next;
-  } else {
-    pp->usiz = usiz;
-    pp->begin += siz;
-  }
-  
-  return d;
+    mpool_t *pp = *p;
+    size_t usiz = mpool_align(pp->usiz + siz);
+    size_t msiz = pp->msiz;
+    mpool_pool_t *d = pp->begin;
+    if (usiz > msiz) {
+        mpool_extend(pp, usiz * 2);
+        pp->next->usiz = usiz;
+        d = pp->next->begin;
+        pp->next->begin += siz;
+        *p = pp->next;
+    } else {
+        pp->usiz = usiz;
+        pp->begin += siz;
+    }
+    
+    return d;
 }
 
 /**
  * release all memory pool
  */
 void mpool_destroy (mpool_t *pool) {
-  for (mpool_t *p=pool->head;p!=NULL;) {
-    mpool_t *current = p;
-    mpool_t *next    = p->next;
-    MPOOL_FREE(current->pool);
-    MPOOL_FREE(current);
-    p = next;
-  }
+    for (mpool_t *p=pool->head;p!=NULL;) {
+        mpool_t *current = p;
+        mpool_t *next    = p->next;
+        MPOOL_FREE(current->pool);
+        MPOOL_FREE(current);
+        p = next;
+    }
 }
 
 /* following is private function */ 
@@ -102,23 +102,23 @@ void mpool_destroy (mpool_t *pool) {
  * extend memory pool
  */
 static void mpool_extend(mpool_t *p, size_t siz) {
-  p->next = mpool_create(siz);
-  p->next->head = p->head;
+    p->next = mpool_create(siz);
+    p->next->head = p->head;
 }
 
 /**
  * align byte boundary
  */
 static size_t mpool_align(size_t siz) {
-  size_t siz_padding = siz % MPOOL_ALIGN_SIZE;
-  size_t siz_aligned;
-  if (siz_padding == 0) {
-    siz_aligned = siz;
-  } else if (siz == siz_padding) {
-    siz_aligned = MPOOL_ALIGN_SIZE;
-  } else {
-    siz_aligned = siz + siz_padding;
-  }
-  return siz_aligned;
+    size_t siz_padding = siz % MPOOL_ALIGN_SIZE;
+    size_t siz_aligned;
+    if (siz_padding == 0) {
+        siz_aligned = siz;
+    } else if (siz == siz_padding) {
+        siz_aligned = MPOOL_ALIGN_SIZE;
+    } else {
+        siz_aligned = siz + siz_padding;
+    }
+    return siz_aligned;
 }
 
--- mpool.h
+++ mpool.h
@@ -1,34 +1,34 @@
 /**
- In short, mpool is distributed under so called "BSD license",
- 
- Copyright (c) 2009-2010 Tatsuhiko Kubo <cubicdaiya@gmail.com>
- All rights reserved.
- 
- Redistribution and use in source and binary forms, with or without modification,
- are permitted provided that the following conditions are met:
- 
-    * Redistributions of source code must retain the above copyright notice,
-    this list of conditions and the following disclaimer.
- 
-    * Redistributions in binary form must reproduce the above copyright notice,
-    this list of conditions and the following disclaimer in the documentation
-    and/or other materials provided with the distribution.
- 
-    * Neither the name of the authors nor the names of its contributors
-    may be used to endorse or promote products derived from this software 
-    without specific prior written permission.
- 
- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
- TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+   In short, mpool is distributed under so called "BSD license",
+   
+   Copyright (c) 2009-2010 Tatsuhiko Kubo <cubicdaiya@gmail.com>
+   All rights reserved.
+   
+   Redistribution and use in source and binary forms, with or without modification,
+   are permitted provided that the following conditions are met:
+   
+   * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+   
+   * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+   
+   * Neither the name of the authors nor the names of its contributors
+   may be used to endorse or promote products derived from this software 
+   without specific prior written permission.
+   
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+   TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 /* written by C99 style */
@@ -44,18 +44,18 @@
 #define MPOOL_ALIGN_SIZE 8
 
 #define MPOOL_MALLOC(p, siz)                    \
-  do {                                          \
-    if (((p) = malloc(siz)) == NULL) {          \
-      fprintf(stderr, "malloc failed");         \
-      exit(-1);                                 \
-    }                                           \
-  } while(false)
+    do {                                        \
+        if (((p) = malloc(siz)) == NULL) {      \
+            fprintf(stderr, "malloc failed");   \
+            exit(-1);                           \
+        }                                       \
+    } while(false)
 
 #define MPOOL_FREE(p)                           \
-  do {                                          \
-    free(p);                                    \
-    (p) = NULL;                                 \
-  } while(false)
+    do {                                        \
+        free(p);                                \
+        (p) = NULL;                             \
+    } while(false)
 
 typedef void mpool_pool_t;
 
@@ -63,12 +63,12 @@ typedef void mpool_pool_t;
  * memory pool structure
  */
 typedef struct mpool_t {
-  mpool_pool_t *pool;       // memory pool field
-  mpool_pool_t *begin;      // data for internal conduct
-  size_t usiz;              // used pool size of each pool
-  size_t msiz;              // max pool size of each pool
-  struct mpool_t *head;     // memory pool's head
-  struct mpool_t *next;     // next memory pool's pointer
+    mpool_pool_t *pool;       // memory pool field
+    mpool_pool_t *begin;      // data for internal conduct
+    size_t usiz;              // used pool size of each pool
+    size_t msiz;              // max pool size of each pool
+    struct mpool_t *head;     // memory pool's head
+    struct mpool_t *next;     // next memory pool's pointer
 } mpool_t;
 
 mpool_t *mpool_create(size_t siz);
